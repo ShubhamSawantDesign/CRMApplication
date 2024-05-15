45925d39541dff345136765ee20e6ae88139df2b
Akash <Akash@icspune.com>
2024-05-15 12:34:25 +0530

    Generate PDF Code

--- app/Http/Controllers/QutationController.php
+++ app/Http/Controllers/QutationController.php
@@ -4,20 +4,21 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Barryvdh\DomPDF\Facade\Pdf;
 use DB;
-use Mpdf\Mpdf;
 use Alert;
 
 class QutationController extends Controller
 {
-    public function createQuatation(Request $request){
+    public function createQuatation(Request $request)
+    {
 
         $customers = DB::table('tbl_customers')->get();
-        return view('createQutation',compact('customers'));
-
+        return view('createQutation', compact('customers'));
     }
 
-    public function doCreateQuotation(Request $request){
+    public function doCreateQuotation(Request $request)
+    {
         $data = $request->all();
         $count = count($data['item']);
         $item = $request->input('item');
@@ -41,12 +42,12 @@ class QutationController extends Controller
             'sgst' => $data['sgst'],
             'other_cost' => $data['other'],
             'final_amount' => $data['final_amount'],
-            'created_by' => Auth::guard('admin')->User()->user_id ,
+            'created_by' => Auth::guard('admin')->User()->user_id,
             'created_at' => now(), // If you want to set created_at and updated_at timestamps
         ]);
 
         /** Insert Items Details */
-        
+
         for ($i = 0; $i < $count; $i++) {
 
             DB::table('tbl_invoice_items')->insert([
@@ -58,38 +59,35 @@ class QutationController extends Controller
                 'created_at' => now(),
                 'created_by' => Auth::guard('admin')->User()->user_id,
             ]);
-
         }
         Alert::success('Quatation Created Successfully');
         return redirect('/listQutation');
     }
 
-    public function listQutation(Request $request){
+    public function listQutation(Request $request)
+    {
         $data = $request->all();
         $invoice_Details = DB::table('tbl_invoice_details')
-        ->join('tbl_customers', 'tbl_invoice_details.customer', '=', 'tbl_customers.id')
-        ->select('tbl_customers.customer_name', 'tbl_invoice_details.*')
-        ->get();
+            ->join('tbl_customers', 'tbl_invoice_details.customer', '=', 'tbl_customers.id')
+            ->select('tbl_customers.customer_name', 'tbl_invoice_details.*')
+            ->get();
 
-         return view('listQutation',compact('invoice_Details'));
+        return view('listQutation', compact('invoice_Details'));
     }
 
     public function generateQuotation($invoice_id)
     {
-        
+
         $invoice_Details = DB::table('tbl_invoice_details')
-        ->join('tbl_customers', 'tbl_invoice_details.customer', '=', 'tbl_customers.id')
-        ->select('tbl_customers.customer_name', 'tbl_customers.address','tbl_invoice_details.*')
-        ->where('tbl_invoice_details.id','=',$invoice_id)
-        ->first();
+            ->join('tbl_customers', 'tbl_invoice_details.customer', '=', 'tbl_customers.id')
+            ->select('tbl_customers.customer_name', 'tbl_customers.address', 'tbl_invoice_details.*')
+            ->where('tbl_invoice_details.id', '=', $invoice_id)
+            ->first();
 
-        $invoice_items = DB::table('tbl_invoice_items')->where('invoice_id','=',$invoice_id)->get();
-    
-        $mpdf = new Mpdf();
-        $html = view('quotation', compact('invoice_Details', 'invoice_items'))->render();
-    
-        $mpdf->WriteHTML($html);
-        $mpdf->Output();
+        $invoice_items = DB::table('tbl_invoice_items')->where('invoice_id', '=', $invoice_id)->get();
+       
+        $pdf = Pdf::loadView('quotation', compact('invoice_items', 'invoice_Details'));
+        $pdf->set_paper('a4', 'portrait');
+        return $pdf->stream();
     }
-
 }
